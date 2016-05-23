4fa48a1213834db29874ddb6f9c8ba64c0bc4b76
Lothar Pfeiler <lpfeiler@Lothars-MBP.fritz.box>
2016-05-23 14:24:36 +0200

    haskell: added ebsmod

--- haskell/ebs.hs
+++ haskell/ebs.hs
@@ -1,13 +1,16 @@
-ebsaux :: Int -> Int -> Int -> Int
-ebsaux a b 0 = a
-ebsaux a b n =
+ebsaux :: (Int -> Int) -> Int -> Int -> Int -> Int
+ebsaux f a b 0 = a
+ebsaux f a b n =
     let
-       a' = case mod n 2 of 1 -> a*b
-                            _ -> a
-       b' = b*b
+       a' = case mod n 2 of 1 -> f (a*b)
+                            _ -> f a
+       b' = f (b*b)
        n' = div n 2
     in
-       ebsaux a' b' n'
+       ebsaux f a' b' n'
 
 ebs :: Int -> Int -> Int
-ebs x = ebsaux 1 x
+ebs x = ebsaux id 1 x
+
+ebsmod :: Int -> Int -> Int -> Int
+ebsmod x n m = ebsaux (`mod` m) 1 x n
