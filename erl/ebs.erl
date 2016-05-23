3a8f1c7ebe95a559111541249e61e57da12131ca
Lothar Pfeiler <lpfeiler@Lothars-MacBook-Pro.fritz.box>
2016-05-23 21:22:08 +0200

    erl: ebs and ebsmod added

---
+++ erl/ebs.erl
@@ -0,0 +1,31 @@
+-module(ebs).
+-export([ebs/2,ebsmod/3]).
+
+%
+%  ebsaux(F,1,X,N) => F(X^N)
+%
+ebsaux(F,A,_,0) -> F(A);
+ebsaux(F,A,B,N) ->
+    A1 = case N rem 2 of
+            1 -> F(A * B);
+            _ -> F(A)
+         end,
+    B1 = F(B * B),
+    N1 = N div 2,
+    ebsaux(F,A1,B1,N1).
+
+
+%
+%  ebs x n => x^n
+%
+ebs(X,N) ->
+    ID = fun (Y) -> Y end,
+    ebsaux(ID, 1, X, N).
+
+
+%
+%  ebsmod(X,N,M) => X^N (mod M)
+%
+ebsmod(X,N,M) ->
+    MOD = fun (A) -> A rem M end,
+    ebsaux(MOD,1,X,N).
