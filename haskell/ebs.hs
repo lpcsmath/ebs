eae97311ee6a58bedd6cb1a1649aaa774628bcf6
Lothar Pfeiler <lpfeiler@Lothars-MacBook-Pro.fritz.box>
2016-05-23 18:12:17 +0200

    haskell: added comments

--- haskell/ebs.hs
+++ haskell/ebs.hs
@@ -1,3 +1,6 @@
+--
+-- ebsaux f 1 b n = f b^n
+--
 ebsaux :: (Int -> Int) -> Int -> Int -> Int -> Int
 ebsaux f a b 0 = a
 ebsaux f a b n =
@@ -9,8 +12,14 @@ ebsaux f a b n =
     in
        ebsaux f a' b' n'
 
+--
+-- ebs x n = x^n
+--
 ebs :: Int -> Int -> Int
 ebs x = ebsaux id 1 x
 
+--
+-- ebsmod x n m = x^n (mod m)
+--
 ebsmod :: Int -> Int -> Int -> Int
 ebsmod x n m = ebsaux (`mod` m) 1 x n
