cc5df4ad11cd023a5af7d30e951b1a9c201423ae
Lothar Pfeiler <lpfeiler@Lothars-MBP.fritz.box>
2016-05-24 17:54:02 +0200

    scala: tests adjusted

--- scala/ebs/src/test/scala/EbsSpec.scala
+++ scala/ebs/src/test/scala/EbsSpec.scala
@@ -2,11 +2,16 @@ import org.scalatest._
 import de.csmath.math.Ebs._
 
 class EbsSpec extends FlatSpec with Matchers {
-  "ebs" should "be correct with positive exponent" in {
+  "ebs" should "be correct with non-negative exponent" in {
     ebs(2,16) should === (65536)
     ebs(2,29) should === (536870912)
     ebs(-2,29) should === (-536870912)
     ebs(2,30) should === (1073741824)
+    ebs(0,0) should === (1)
+    ebs(2,0) should === (1)
+    ebs(0,1) should === (0)
+    ebs(0,1000) should === (0)
+    ebs(1,1000) should === (1)
   }
   
   "ebs" should "fail with negative exponent" in {
